d4ea9fdc957083bafff1131956b061e948e3b8ec
gdha <gratien.dhaese@gmail.com>
2025-09-22 14:32:18 +0200

    replace TIER by ENVIRONMENT

--- alert.sh
+++ alert.sh
@@ -3,7 +3,7 @@
 # PURPOSE: send an alert to MS Teams PowerAutomate channel
 # Author: Gratien D'haese
 # Version: 1.0
-# date: 16/Sep/2025
+# date: 22/Sep/2025
 
 #############
 # VARIABLES #
@@ -15,16 +15,14 @@ HOSTNAME=$(/usr/bin/hostname)
 
 # Get the tier value to which this system belongs (sandbox, developmnt, qa , uat or production)
 # Most likely the /etc/tier file only exist on Treasury Linux systems.
-[[ -f /etc/tier ]] && TIER=$(cat /etc/tier)
+[[ -f /etc/tier ]] && ENVIRONMENT=$(cat /etc/tier)
 
-# When the TIER part was commented out in the configuration file we have the next block to rescue
-if [[ "$TIER" = "" ]]; then
-  [[ -x /bin/ohai ]] && TIER=$(/bin/ohai | grep -i scm_appbranch | cut -d\" -f4)
+# When the ENVIRONMENT part was commented out in the configuration file we have the next block to rescue
+if [[ "$ENVIRONMENT" = "" ]]; then
+  [[ -x /bin/ohai ]] && ENVIRONMENT=$(/bin/ohai | grep -i scm_appbranch | cut -d\" -f4)
 fi
 
-export TIER
-
-CONFIG="alert.conf"
+CONFIG="/etc/alert.conf"
 TITLE=""
 BODY=""
 FILE=""
@@ -33,7 +31,6 @@ IMAGE_URL="https://www.energise.co.nz/wp-content/uploads/2016/04/Prove-you-are-n
 # ReaR logo:
 # IMAGE_URL="https://it3.be/images/logo/rear_logo_100.png"
 WEBHOOK_URL=""
-ENVIRONMENT=""
 
 #############
 # Functions #
@@ -42,11 +39,11 @@ ENVIRONMENT=""
 function show_usage ()
 {
     # input value: exit code
-    echo "Usage: alert [-t|--tier] tier [[-c|--config] configuration-file] [[-T|--title] \"TITLE line\"] [[-b|--body] \"body text\"] [[-f|--file] file for body text] [[-i|--image \"URL-of-picture\"]" 
+    echo "Usage: alert [-e|--environment] tier [[-c|--config] configuration-file] [[-t|--title] \"TITLE line\"] [[-b|--body] \"body text\"] [[-f|--file] file for body text] [[-i|--image \"URL-of-picture\"]" 
     printf "\nAvailable options:\n"
-    echo "-t|--tier         tier e.g. sbx, dev, qa, uat or prd (optional)"
+    echo "-e|--environment  tier e.g. sandbox, developmnet, qa, uat or production (optional)"
     echo "-c|--config       configuration: is configuration file file (optional - default $CONFIG)"
-    echo "-T|--title        title message (required)"
+    echo "-t|--title        title message (required)"
     echo "-b|--body         body text (optional when --file is used)"
     echo "-f|--file         read body text from file or stdin (required when --body is not used)"
     echo "-i|--image        Logo graph URL (optional)"
@@ -69,6 +66,7 @@ function error
 umask 022
 
 # Source the configuration file if it exists
+# The config file should contain the settings WEBHOOK_URL and ENVIRONMENT
 if [[ -f /etc/${PROGNAME}.conf ]]  ; then
     CONFIG="/etc/${PROGNAME}.conf"
 fi
@@ -80,7 +78,7 @@ fi
 # Examples at https://davetang.org/muse/2023/01/31/bash-script-that-accepts-short-long-and-positional-arguments/
 help_note_text="Use '$PROGNAME --help' or 'man $PROGNAME' for more information."
 
-if ! OPTS="$( getopt -n $PROGNAME -o "t:c:T:b:f:i:hv" -l "tier:,config:,title:,body:,file:,image:,help,version" -- "$@" )" ; then
+if ! OPTS="$( getopt -n $PROGNAME -o "e:c:t:b:f:i:hv" -l "environment:,config:,title:,body:,file:,image:,help,version" -- "$@" )" ; then
     echo "$help_note_text"
     exit 1
 fi
@@ -94,13 +92,13 @@ while true ; do
         (-v|--version)
             echo "$PROGNAME,v$VERSION"
             exit 0                ;;
-        (-t|--tier)
+        (-e|--environment)
             if [[ "$2" == -* ]] ; then
-                echo "-t requires an argument."
+                echo "-e requires an argument."
                 echo "$help_note_text"
                 exit 1
             fi
-            TIER="$2"
+            ENVIRONMENT="$2"
             shift                 ;;
         (-c|--config)
             if [[ "$2" == -* ]] ; then
@@ -110,7 +108,7 @@ while true ; do
             fi
             CONFIG="$2"
             shift                 ;;
-        (-T|--title)
+        (-t|--title)
             if [[ "$2" == -* ]] ; then
                 echo "-T requires an argument."
                 echo "$help_note_text"
@@ -165,9 +163,9 @@ else
     source "$CONFIG"
 fi
 
-# Make sure that the TIER value is lowercase for the remaining part of this program
-TIER="$( echo ${TIER,,} )"
-if [[ -z "$TIER" ]] ; then
+# Make sure that the ENVIRONMENT value is lowercase for the remaining part of this program
+ENVIRONMENT="$( echo ${ENVIRONMENT,,} )"
+if [[ -z "$ENVIRONMENT" ]] ; then
     show_usage 1
 fi
 # INFO: correct tier value is saved under the ENVIRONMENT variable
@@ -188,7 +186,6 @@ fi
 
 # The BODY text may not contain " as it screw up JSON
 BODY="$(echo $BODY | sed -e 's/"/:/g')"
-
 
 color=Warning
 HEADER="Alert on $(hostname -s) ($ENVIRONMENT)"
